1d2945290e97a201b5afeaf2be30a1ecde8cc94a
ihamuma <you@example.com>
2022-01-12 17:24:12 +0200

    add errorHandler, requestLogger, unknownEndpoint

--- index.js
+++ index.js
@@ -5,69 +5,92 @@ const cors = require('cors')
 const morgan = require('morgan')
 const app = express()
 
-app.use(express.json())
 app.use(express.static('build'))
+app.use(express.json())
 app.use(cors())
 morgan.token('data', (req, res) => {
   return JSON.stringify(req.body)
 })
+
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data' ))
-  
-  app.get('/api/persons', (request, response) => {
-    Person.find({}).then(people => {
-      response.json(people)
-    })
-  })
 
-  app.get('/api/persons/:id', (request, response) => {
-    Person.findById(request.params.id)
-      .then(person => {
-        if (person) {
-          response.json(person)
-        } else {
-          response.status(404).end
-        }        
-    })
-    .catch(error => {
-      console.log(error)
-      response.status(400).send({ error: 'malformatted id'})
-    })
-  })
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
 
-  app.get('/info', (request, response) => {
-    response.send(`<p>Phonebook has info for ${persons.length} people</p>
-    <p>${new Date()}</p>`)
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
+app.use(requestLogger)
+
+app.get('/api/persons', (request, response) => {
+  Person.find({}).then(people => {
+    response.json(people)
+  })
 })
 
-  app.delete('/api/persons/:id', (request, response) => {
-    Person.findByIdAndRemove(request.params.id)
-      .then(result => {
-        response.status(204).end()
-      })
-      .catch(error => {
-        console.log(error)
-        response.status(400).send({ error: 'user already deleted from server' })
-      })
+app.get('/api/persons/:id', (request, response) => {
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end
+      }        
   })
+  .catch(error => next(error))
+})
 
-  app.post('/api/persons', (request, response) => {
-    const body = request.body
+app.get('/info', (request, response) => {
+  response.send(`<p>Phonebook has info for ${persons.length} people</p>
+  <p>${new Date()}</p>`)
+})
 
-    if (body.name === undefined) {
-      return response.status(400).json({ error: 'name missing' })
-    }
-
-    const person = new Person({
-      name: body.name,
-      number: body.number
-    })    
-    person.save().then(savedPerson => {
-      response.json(savedPerson)
+app.delete('/api/persons/:id', (request, response) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
     })
+    .catch(error => next(error))
+})
 
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  if (body.name === undefined) {
+    return response.status(400).json({ error: 'name missing' })
+  }
+
+  const person = new Person({
+    name: body.name,
+    number: body.number
+  })    
+  person.save().then(savedPerson => {
+    response.json(savedPerson)
   })
 
-  const PORT = process.env.PORT
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-  })
+})
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+app.use(errorHandler)
+
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
