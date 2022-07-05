937046a71b9aa7ee1c39b7fec01e2e41256cd03e
ihamuma <you@example.com>
2022-07-05 21:26:03 +0300

    eslint stuff

--- mongo.js
+++ mongo.js
@@ -6,14 +6,14 @@ if (process.argv.length < 3) {
   process.exit(1)
 }
 
-if (process.argv.length == 4 ) {
-    console.log('Please provide a name and a number as arguments: node mongo.js <password> <name> <number>')
-    process.exit(1)
+if (process.argv.length === 4 ) {
+  console.log('Please provide a name and a number as arguments: node mongo.js <password> <name> <number>')
+  process.exit(1)
 }
 
 if (process.argv.length > 5 ) {
-    console.log('Please do not provide additional arguments. Arguments: node mongo.js <password> <name> <number>')
-    process.exit(1)
+  console.log('Please do not provide additional arguments. Arguments: node mongo.js <password> <name> <number>')
+  process.exit(1)
 }
 
 const url = process.env.MONGODB_URI
@@ -36,33 +36,33 @@ const personSchema = new mongoose.Schema({
 console.log('schema defined in mongo.js')
 
 personSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 
 const Person = mongoose.model('Person', personSchema)
 
-if (process.argv.length == 3) {
-    Person.find({})
+if (process.argv.length === 3) {
+  Person.find({})
     .then(console.log('Phonebook:'))
     .then(result => {
-        result.forEach(person => {
-          console.log(person.name, '', person.number)
-        })
-        mongoose.connection.close()
+      result.forEach(person => {
+        console.log(person.name, '', person.number)
       })
+      mongoose.connection.close()
+    })
 }
 
-if (process.argv.length == 5 ) {
-    const person = new Person({
-        name: process.argv[3],
-        number: process.argv[4],
-        })
-    person.save()
-      .then(result => {
+if (process.argv.length === 5 ) {
+  const person = new Person({
+    name: process.argv[3],
+    number: process.argv[4],
+  })
+  person.save()
+    .then(() => {
       console.log(`Added ${person.name}, number ${person.number}, to phonebook`)
       mongoose.connection.close()
     })
